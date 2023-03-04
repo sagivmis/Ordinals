769963aef53a7377d9bbd88a7e6990ffaec4cd71
Sagiv Mishaan <sagiv505@gmail.com>
2023-03-04 16:19:20 +0200

    types

---
+++ src/types/general.types.ts
@@ -0,0 +1,26 @@
+export type ItemHistoryRow = {
+  price: number
+  date: string
+  from: string
+}
+
+export type CollectionItem = {
+  id: number
+  price: number
+  history: ItemHistoryRow[]
+  owner: string
+  image: string
+}
+
+export type CollectionType = {
+  id: number
+  name: string
+  floorPrice: number
+  volume: number
+  totalSupply: number
+  image: string
+  data?: CollectionItem[]
+  collectionOwner: string
+}
+
+export type CollectionInfo = { field: string; value: number | string }
