4cf0636745171bd4e249bd70af18e76f88e4c4a8
Sagiv Mishaan <sagiv505@gmail.com>
2023-03-30 17:42:04 +0300

    Undo typing input button

--- src/components/OrdinalSearch/OrdinalSearch.tsx
+++ src/components/OrdinalSearch/OrdinalSearch.tsx
@@ -1,8 +1,9 @@
-import React, { Dispatch, SetStateAction } from "react"
+import React, { Dispatch, SetStateAction, useState } from "react"
 import Input from "@mui/material/Input"
 import searchIcon from "../../assets/searchIcon.png"
 import InputAdornment from "@mui/material/InputAdornment"
 import clsx from "clsx"
+import { CloseRounded } from "@mui/icons-material"
 
 interface ISearch {
   mode: "collection" | "item"
@@ -13,6 +14,8 @@ interface ISearch {
 
 const OrdinalSearch = (props: ISearch) => {
   const { mode, open, setOpen, setText } = props
+  const [currentText, setCurrentText] = useState("")
+
   return (
     <div className={clsx("search-container", open ? "open" : "")}>
       <Input
@@ -27,9 +30,24 @@ const OrdinalSearch = (props: ISearch) => {
             />
           </InputAdornment>
         }
+        endAdornment={
+          <InputAdornment position='start'>
+            {open && (
+              <CloseRounded
+                className='undo-input'
+                onClick={() => {
+                  setText("")
+                  setCurrentText("")
+                }}
+              />
+            )}
+          </InputAdornment>
+        }
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
           setText(event.target.value)
+          setCurrentText(event.target.value)
         }}
+        value={currentText}
         disableUnderline
         placeholder={`Search ${mode}s...`}
       />
